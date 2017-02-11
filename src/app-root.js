3ddaf2139e5957132fd2ef7f2a4b8a409f0415cb
Igor N. Dultsev <yhaskell@live.com>
2017-02-11 22:02:03 +0700

    add react-app stuff

---
+++ src/app-root.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const AppRoot = () => (
+    <div className="container" style={{ marginTop: "80px" }}>
+        <h1>Task manager</h1>
+        <p className="lead">written by <a href="https://twitter.com/yhaskell">@yhaskell</a> to learn react/redux</p>
+    </div>
+)
+
+export default AppRoot;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRoot from './app-root';
+import './index.css';
+
+ReactDOM.render(
+  <AppRoot />,
+  document.getElementById('root')
+);
